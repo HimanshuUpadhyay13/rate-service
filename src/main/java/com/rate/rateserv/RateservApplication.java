abf9e27a99c913a9fe4aa415fc3025c35dccfa8c
hiupadhyay <hiupadhyay@paypal.com>
2021-05-22 02:06:53 +0530

    [TASK-01]Setting up Repo

---
+++ src/main/java/com/rate/rateserv/RateservApplication.java
@@ -0,0 +1,13 @@
+package com.rate.rateserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rate/rateserv/RateservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rate.rateserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
